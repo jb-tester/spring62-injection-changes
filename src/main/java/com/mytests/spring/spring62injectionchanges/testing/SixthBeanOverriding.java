0f574e14dc5722b533e350f1922bdd5640c56303
irina.petrovskaya <irina.petrovskaya@jetbrains.com>
2025-02-25 18:34:36 +0100

    @TestBean: - not-existing beans - static methods from external class

---
+++ src/main/java/com/mytests/spring/spring62injectionchanges/testing/SixthBeanOverriding.java
@@ -0,0 +1,9 @@
+package com.mytests.spring.spring62injectionchanges.testing;
+
+
+public class SixthBeanOverriding {
+
+    public static SixthBeanToOverride testBean6(){
+        return new SixthBeanToOverride("test");
+    }
+}
