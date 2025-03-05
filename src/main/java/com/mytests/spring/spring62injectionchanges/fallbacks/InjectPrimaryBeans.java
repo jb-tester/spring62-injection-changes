e88b1ebd6c7c882bac632f4e889fde1dd0626330
irina.petrovskaya <irina.petrovskaya@jetbrains.com>
2025-03-05 18:40:30 +0100

    check qualified injections of not primary beans

--- src/main/java/com/mytests/spring/spring62injectionchanges/fallbacks/InjectPrimaryBeans.java
+++ src/main/java/com/mytests/spring/spring62injectionchanges/fallbacks/InjectPrimaryBeans.java
@@ -13,14 +13,26 @@ public class InjectPrimaryBeans {
     private final Bean1 mybean1;
     private final Bean2 mybean2;
     private final Bean3 mybean3;
+    private final Bean3 mybean33;
+    private final Bean1 mybean11;
+    private final Bean2 mybean20;
 
     // for Bean1-type parameter the single candidate should be reported - the one that doesn't have the @Fallback annotation
     // for Bean2-type parameter the single candidate should be reported - the one annotated with @Primary
     // for Bean3-type parameter the single candidate should be reported - the one that has #defaultCandidate=true
-    public InjectPrimaryBeans(Bean1 mybean1, Bean2 mybean2, Bean3 mybean3) {
-        this.mybean1 = mybean1;
-        this.mybean2 = mybean2;
-        this.mybean3 = mybean3;
+    // for Bean1-type parameter that has the matching name 'mybean11' autowiring by name should be performed, though this bean has @Fallback annotation
+    // for Bean2-type parameter that has the matching name 'mybean20' autowiring by name should be performed, though this bean doesn't have @Primary annotation
+    // for Bean3-type parameter that has the matching name 'mybean33' autowiring by name should be performed, though this bean has #defaultCandidate = false
+    public InjectPrimaryBeans(Bean1 mybean1, Bean2 mybean2, Bean3 mybean3, Bean3 mybean33, Bean1 mybean11, Bean2 mybean20) {
+        // primary/defaults:
+        this.mybean1 = mybean1; // ok, navigates to 'mybean13' that doesn't have @Fallback
+        this.mybean2 = mybean2; // ok, navigates to 'mybean21' bean that is @Primary-annotated
+        this.mybean3 = mybean3; // ok, navigates to 'mybean32' bean that has #defaultCandidate = true
+        // by name:
+        this.mybean11 = mybean11; // ok
+        this.mybean20 = mybean20; // no error, but navigates to default (mybean21) bean that is @Primary-annotated
+        this.mybean33 = mybean33; // false autowiring error is shown
+
     }
 
     public void displayBeans() {
